19646ea60a7522cc8f8c48cfff50810fbdbd3307
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-30 11:59:15 -0500

    finished step 7.1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Post.all
+    @money = 18405978134059782349057823490587230495872039423409587
+  end
+
+  def show
+    @article = Post.find(params[:id])
   end
 end
